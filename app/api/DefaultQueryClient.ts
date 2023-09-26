6868c58f2e1109a1e76a509bbc97637e7d15e829
FuRy30 <adam.jan.duda.krk@gmail.com>
2023-09-26 09:04:49 +0200

    Query Client 401

--- app/api/DefaultQueryClient.ts
+++ app/api/DefaultQueryClient.ts
@@ -1,4 +1,6 @@
 import { QueryCache, QueryClient } from "@tanstack/react-query";
+import AuthState from "../store/auth/AuthState";
+import { redirect } from "next/navigation";
 
 const DefaultQueryClient = new QueryClient({
     defaultOptions: {
@@ -13,6 +15,10 @@ const DefaultQueryClient = new QueryClient({
             if (!query.meta?.ignore401 && err.response?.status == 401) {
                 DefaultQueryClient.removeQueries();
                 // redirect to login
+                localStorage.removeItem('token');
+                localStorage.removeItem('expiration');
+                AuthState.IsLogged = false;
+                redirect('/auth/login');
             }
             else if (err.response?.status == 500) {
                 // error
